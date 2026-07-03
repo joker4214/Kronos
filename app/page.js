f6a5d6b3f1934dde99a4954543b00843f7a0138d
Jason <gregory.jasonpaul@gmail.com>
2026-07-03 00:04:48 -0400

    Add Home component to page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Kronos Running</h1>
+      <p>Content generation agent is live.</p>
+    </div>
+  );
+}
